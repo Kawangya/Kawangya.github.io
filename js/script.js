b1fb37ac338c30566374141545c2746b89538e01
卡王吖 <127932394+Kawangya@users.noreply.github.com>
2025-11-12 19:20:20 +0800

    Update script.js via web editor

--- js/script.js
+++ js/script.js
@@ -82,7 +82,7 @@ document.querySelectorAll('img').forEach(img => {
         overlay.style.cssText = `
             position: fixed;
             top: 0; left: 0; right: 0; bottom: 0;
-            background: rgba(0,0,0,0.9); // 半透明黑色背景
+            background: rgba(0,0,0,0.5); // 半透明黑色背景
             display: flex;
             align-items: center;
             justify-content: center;
